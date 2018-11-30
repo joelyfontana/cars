6a3bc037223d406cc09ec38f44ead8433e122cd3
Joely Fontana <fontana@chapman.edu>
2018-11-30 11:50:41 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#cars.sh
+#Joely Fontana
+
+echo "My Old Cars Inventory System"
+read My_Old_Cars
+
+while echo $My_Old_Cars
+do
+	echo "Enter a Car: Press 1"
+	echo "List Cars: Press 2"
+	echo "Quit: Press Q"
+	 
